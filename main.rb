9ec0ad3881170f948b866e7ca0293ac573cfd5b9
Galaad-Gauthier <coontail7@gmail.com>
2013-04-23 00:45:21 +0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -34,7 +34,8 @@ end
 while true
 	nb_args = OPCODES[$cpu.ram[$cpu.cpu[:compteur]]][:len].to_i
 	$cpu.cpu[:compteur_new] += nb_args
-	$cpu.send OPCODES[$cpu.ram[$cpu.cpu[:compteur]]][:opcodes].downcase, $cpu.ram[$cpu.cpu[:compteur]], $cpu.ram[$cpu.cpu[:compteur]+1], $cpu.ram[$cpu.cpu[:compteur]+2]
+	$cpu.send OPCODES[$cpu.ram[$cpu.cpu[:compteur]]][:opcodes].downcase, 
+		$cpu.ram[$cpu.cpu[:compteur]], $cpu.ram[$cpu.cpu[:compteur]+1], $cpu.ram[$cpu.cpu[:compteur]+2]
 	ppu_exec(OPCODES[$cpu.ram[$cpu.cpu[:compteur]]][:tim].to_i)
 	#puts "#{$cpu.ram[$cpu.cpu[:compteur]+1]}, #{$cpu.ram[$cpu.cpu[:compteur]+2]}"
 	#puts "Nom de l'instruct : #{OPCODES[$cpu.ram[$cpu.cpu[:compteur]]][:opcodes]}"
