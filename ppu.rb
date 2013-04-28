534c4cb7407eecf9a382a02a71f46ce6c0963eaf
Galaad-Gauthier <coontail7@gmail.com>
2013-04-28 19:56:19 +0300

    Update ppu.rb

--- ppu.rb
+++ ppu.rb
@@ -1,51 +1,13 @@
 Registers = [0x2000,0x2001,0x2002,0x2003,0x2004,0x2005,0x2006,0x2007,0x4014]
 
-class Ppu
-<<<<<<< HEAD
-attr_accessor :ppu
-attr_accessor :registers
-attr_accessor :screen
-
-
-def initialize()
-	@ppu = [0]*0x4000
-	@registers = {}
-	Registers.each {|adresse| @registers[adresse] = 0}
-	init_affichage
-end
-
-
-def init_affichage()
-	SDL.init SDL::INIT_VIDEO
-	@screen = SDL::set_video_mode 256, 240, 32, SDL::SWSURFACE
-	reset_pixel
-end
-
-
-def reset_pixel()
-	@x = @y = 0
-end
-
-
-def get_bit(liste_octets,ligne,bit)
-	return (liste_octets[ligne].bit?(7-bit) | liste_octets[ligne+8].bit?(7-bit) << 1)
-end
-
-def get_pixel(x,y)
-	set_tables
-	pos_tile = (y/8)*32+(x/8)
-	index = @table_nommage_screen[pos_tile] * 16
-	pixel = get_bit(@screen_pattern_table[index..index+15],y%8,x%8)
-	return pixel
-=======
+class Ppu	
 	attr_accessor :ppu
 	attr_accessor :registers
 	attr_accessor :screen
->>>>>>> 124c499b84f9088ffdb0d58c7131459d5d723e50
 	
 	
 	def initialize()
-		@ppu = [0]*100000
+		@ppu = [0]*0x4000
 		@registers = {}
 		Registers.each {|adresse| @registers[adresse] = 0}
 		init_affichage
@@ -63,36 +25,73 @@ def get_pixel(x,y)
 		@x = @y = 0
 	end
 	
-<<<<<<< HEAD
-	@sprite_size = @registers[0x2000].bit?(5)
-
-	@table_nommage_screen = @ppu[0x2000..0x23C0]   if @registers[0x2000].bit?(1) == 0 and @registers[0x2000].bit?(0) == 0
-	@table_nommage_screen = @ppu[0x2400..0x27C0]   if @registers[0x2000].bit?(1) == 0 and @registers[0x2000].bit?(0) == 1
-	@table_nommage_screen = @ppu[0x2800..0x2BC0]   if @registers[0x2000].bit?(1) == 1 and @registers[0x2000].bit?(0) == 0
-	@table_nommage_screen = @ppu[0x2C00..0x2FC0]   if @registers[0x2000].bit?(1) == 1 and @registers[0x2000].bit?(0) == 1
 	
-
-end
-
-
-def set_vblank()
-	@registers[0x2002] = @registers[0x2002].set_bit(7) #Set du Vblank
-end
-
-def clear_vblank()
-	@registers[0x2002] = @registers[0x2002].clear_bit(7) #Clear du Vblank
-end
-
-
-def draw_screen()
-	@screen.putPixel(@x,@y,color?(get_pixel(@x,@y)))
-	@x+=1
-	if @x>255
-		@x = 0
-		@y+=1
-	elsif @y>239
-		reset_pixel
-=======
+	def get_bit(liste_octets,ligne,bit)
+		return (liste_octets[ligne].bit?(7-bit) | liste_octets[ligne+8].bit?(7-bit) << 1)
+	end
+	
+	def get_pixel(x,y)
+		set_tables
+		pos_tile = (y/8)*32+(x/8)
+		index = @table_nommage_screen[pos_tile] * 16
+		pixel = get_bit(@screen_pattern_table[index..index+15],y%8,x%8)
+		return pixel
+	=======
+		attr_accessor :ppu
+		attr_accessor :registers
+		attr_accessor :screen
+	>>>>>>> 124c499b84f9088ffdb0d58c7131459d5d723e50
+		
+		
+		def initialize()
+			@ppu = [0]*100000
+			@registers = {}
+			Registers.each {|adresse| @registers[adresse] = 0}
+			init_affichage
+		end
+		
+		
+		def init_affichage()
+			SDL.init SDL::INIT_VIDEO
+			@screen = SDL::set_video_mode 256, 240, 32, SDL::SWSURFACE
+			reset_pixel
+		end
+		
+		
+		def reset_pixel()
+			@x = @y = 0
+		end
+		
+	<<<<<<< HEAD
+		@sprite_size = @registers[0x2000].bit?(5)
+	
+		@table_nommage_screen = @ppu[0x2000..0x23C0]   if @registers[0x2000].bit?(1) == 0 and @registers[0x2000].bit?(0) == 0
+		@table_nommage_screen = @ppu[0x2400..0x27C0]   if @registers[0x2000].bit?(1) == 0 and @registers[0x2000].bit?(0) == 1
+		@table_nommage_screen = @ppu[0x2800..0x2BC0]   if @registers[0x2000].bit?(1) == 1 and @registers[0x2000].bit?(0) == 0
+		@table_nommage_screen = @ppu[0x2C00..0x2FC0]   if @registers[0x2000].bit?(1) == 1 and @registers[0x2000].bit?(0) == 1
+		
+	
+	end
+	
+	
+	def set_vblank()
+		@registers[0x2002] = @registers[0x2002].set_bit(7) #Set du Vblank
+	end
+	
+	def clear_vblank()
+		@registers[0x2002] = @registers[0x2002].clear_bit(7) #Clear du Vblank
+	end
+	
+	
+	def draw_screen()
+		@screen.putPixel(@x,@y,color?(get_pixel(@x,@y)))
+		@x+=1
+		if @x>255
+			@x = 0
+			@y+=1
+		elsif @y>239
+			reset_pixel
+		
 	
 	def get_bit(liste_octets,ligne,bit)
 		return (liste_octets[ligne].bit?(7-bit) | liste_octets[ligne+8].bit?(7-bit) << 1)
@@ -165,8 +164,6 @@ def draw_screen()
 			reset_pixel
 			@screen.flip
 		end
-	
->>>>>>> 124c499b84f9088ffdb0d58c7131459d5d723e50
 	end
 
 end
